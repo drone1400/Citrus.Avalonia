c14b26210d7acd6ab4d74897ec9339bd38a95917
drone1400 <>
2025-03-30 17:07:32 +0300

    Added some missing controls - added ButotnSpinner - added RepeatButton - fixed NumericUpDown - changes to Sandbox UI

---
+++ src/Citrus.Avalonia.Sandbox/Views/SpinnerButtonControls.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Citrus.Avalonia.Sandbox.Views;
+
+public partial class SpinnerButtonControls : UserControl {
+    public SpinnerButtonControls() {
+        InitializeComponent();
+    }
+}
+
